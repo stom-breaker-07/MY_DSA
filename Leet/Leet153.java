ddd90ed020f95badf9256cfc8a610325ebf58ff0
stom-breaker-07 <chinmay.l10m8@gmail.com>
2024-12-17 00:21:54 +0530

    Day36

---
+++ Leet/Leet153.java
@@ -0,0 +1,19 @@
+package MY_DSA.Leet;
+
+public class Leet153 {
+
+
+}
+
+
+//liner search
+//public int findMin(int[] nums) {
+//    int min=Integer.MAX_VALUE,curr=0;
+//    for(int i=0;i<nums.length;i++){
+//        curr =nums[i];
+//        if(nums[i]<min){
+//            min=nums[i];
+//        }
+//    }
+//    return min ;
+//}
