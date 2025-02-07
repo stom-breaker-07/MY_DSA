79310bd427dc625f2fb92272b0b6b91915bb5577
stom-breaker-07 <chinmay.l10m8@gmail.com>
2025-02-07 23:19:35 +0530

    Feb07

---
+++ Leet/Leet080.java
@@ -0,0 +1,29 @@
+package MY_DSA.Leet;
+
+public class Leet080 {
+    public int removeDuplicates(int[] nums) {
+        int idx=0;
+        for(int num : nums){
+            if(idx<2 || nums[idx-2]!=num){
+                nums[idx]=num;
+                idx++;
+            }
+        }
+        return idx;
+    }
+}
+
+/*Given an integer array nums sorted in non-decreasing order, remove some duplicates
+ in-place such that each unique element appears at most twice. The
+  relative order of the elements should be kept the same.
+
+Since it is impossible to change the length of the array in some languages, you must
+instead have the result be placed in the first part of the array nums. More formally,
+ if there are k elements after removing the duplicates,
+ then the first k elements of nums should hold the final result.
+  It does not matter what you leave beyond the first k elements.
+
+Return k after placing the final result in the first k slots of nums.
+
+Do not allocate extra space for another array. You must do this by modifying the input array
+ in-place with O(1) extra memory.*/
