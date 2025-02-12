dff9fbc2d7d5e01b5bec9a119a27a13895751be2
stom-breaker-07 <chinmay.l10m8@gmail.com>
2025-02-12 14:34:17 +0530

    Feb12

---
+++ Leet/Leet561.java
@@ -0,0 +1,28 @@
+package MY_DSA.Leet;
+
+import java.util.Arrays;
+
+public class Leet561 {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int max=0,i=0;
+        while(i < nums.length){
+            max+=nums[i];
+            i=i+2;
+        }
+        return max;
+    }
+}
+
+/*Input: nums = [1,4,3,2]
+Output: 4
+Explanation: All possible pairings (ignoring the ordering of elements) are:
+1. (1, 4), (2, 3) -> min(1, 4) + min(2, 3) = 1 + 2 = 3
+2. (1, 3), (2, 4) -> min(1, 3) + min(2, 4) = 1 + 2 = 3
+3. (1, 2), (3, 4) -> min(1, 2) + min(3, 4) = 1 + 3 = 4
+So the maximum possible sum is 4.
+Example 2:
+
+Input: nums = [6,2,6,5,1,2]
+Output: 9
+Explanation: The optimal pairing is (2, 1), (2, 5), (6, 6). min(2, 1) + min(2, 5) + min(6, 6) = 1 + 2 + 6 = 9.*/
