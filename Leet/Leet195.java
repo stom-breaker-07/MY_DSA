4e69657ffcd900b500089eb371d07c4fe95f1681
stom-breaker-07 <chinmay.l10m8@gmail.com>
2025-02-17 07:35:00 +0530

    Feb17

---
+++ Leet/Leet195.java
@@ -0,0 +1,14 @@
+package MY_DSA.Leet;
+
+public class Leet195 {
+    /*# Read from the file file.txt and output the tenth line to stdout.
+count=0
+
+while IFS= read -r line; do
+    count=$((count + 1))  # Increment counter
+    if [ $count -eq 10 ]; then
+        echo "$line"  # Print the 10th line
+        break  # Exit the loop once the 10th line is found
+    fi
+done < file.txt*/
+}
