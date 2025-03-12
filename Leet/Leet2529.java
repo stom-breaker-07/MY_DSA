178a9630842cbdb666504b78a287c5d7b37576ac
stom-breaker-07 <chinmay.l10m8@gmail.com>
2025-03-13 00:08:33 +0530

    Mar13

---
+++ Leet/Leet2529.java
@@ -0,0 +1,31 @@
+package MY_DSA.Leet;
+
+public class Leet2529 {
+    public int maximumCount(int[] nums) {
+        int cntP=0,cntN=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>0){
+                cntP++;
+            }
+            else if(nums[i]<0){
+                cntN++;
+            }
+        }
+        return Math.max(cntP,cntN);
+    }
+}
+/*Example 1:
+
+Input: nums = [-2,-1,-1,1,2,3]
+Output: 3
+Explanation: There are 3 positive integers and 3 negative integers. The maximum count among them is 3.
+Example 2:
+
+Input: nums = [-3,-2,-1,0,0,1,2]
+Output: 3
+Explanation: There are 2 positive integers and 3 negative integers. The maximum count among them is 3.
+Example 3:
+
+Input: nums = [5,20,66,1314]
+Output: 4
+Explanation: There are 4 positive integers and 0 negative integers. The maximum count among them is 4.*/
