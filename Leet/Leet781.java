3d6b638848229199a9efbd02bf5e60fad7b979b6
stom-breaker-07 <chinmay.l10m8@gmail.com>
2025-04-21 20:59:49 +0530

    Apr21

---
+++ Leet/Leet781.java
@@ -0,0 +1,24 @@
+package MY_DSA.Leet;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Leet781 {
+    public int numRabbits(int[] answers) {
+        Map<Integer, Integer> freq = new HashMap<>();
+
+        for (int a : answers) {
+            freq.put(a, freq.getOrDefault(a, 0) + 1);
+        }
+
+        int count = 0;
+        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
+            int k = entry.getKey();
+            int v = entry.getValue();
+            int groupSize = k + 1;
+            int groups = (v + k) / groupSize;
+            count += groups * groupSize;
+        }
+        return count;
+    }
+}
