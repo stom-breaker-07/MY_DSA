a291f8162525854146175162ec2693bc1de64046
stom-breaker-07 <chinmay.l10m8@gmail.com>
2025-04-27 21:08:17 +0530

    Apr27

---
+++ Leet/Leet2444.java
@@ -0,0 +1,17 @@
+package MY_DSA.Leet;
+
+public class Leet2444 {
+    public long countSubarrays(int[] nums, int minK, int maxK) {
+        int left = -1, min = -1, max = -1, n = nums.length;
+        long count = 0l;
+        for (int i = 0; i < n; i++)
+            if (nums[i] < minK || nums[i] > maxK)
+                left = min = max = i;
+            else {
+                min = nums[i] == minK ? i : min;
+                max = nums[i] == maxK ? i : max;
+                count += (min < max ? min : max) - left;
+            }
+        return count;
+    }
+}
