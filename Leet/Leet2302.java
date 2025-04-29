c793152500d05a9cb316a2b68361c40267de8373
stom-breaker-07 <chinmay.l10m8@gmail.com>
2025-04-29 20:51:42 +0530

    Apr29

---
+++ Leet/Leet2302.java
@@ -0,0 +1,18 @@
+package MY_DSA.Leet;
+
+public class Leet2302 {
+    public long countSubarrays(int[] nums, long k) {
+        long res = 0, partSum = 0;
+        int left = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            partSum += nums[right];
+            while (partSum * (right - left + 1) >= k) {
+                partSum -= nums[left++];
+            }
+            res += right - left + 1;
+        }
+
+        return res;
+    }
+}
