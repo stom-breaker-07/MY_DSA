64db47fb27f3fc1145773155f17c2ae602accab5
stom-breaker-07 <chinmay.l10m8@gmail.com>
2025-05-09 18:29:35 +0530

    May 09

--- Leet/Leet234.java
+++ Leet/Leet234.java
@@ -1,2 +1,35 @@
-package MY_DSA.Leet;public class Leet234 {
+package MY_DSA.Leet;
+
+import java.util.ArrayList;
+
+public class Leet234 {
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+        public boolean isPalindrome(ListNode head) {
+            ListNode Link=head;
+            ArrayList<Integer> arr=new ArrayList<>();
+
+            while(Link!=null){
+                arr.add(Link.val);
+                Link=Link.next;
+            }
+
+            Link=head;
+            int rIdx=arr.size()-1;
+
+            while(Link!=null){
+                if(Link.val !=arr.get(rIdx--)){
+                    return false;
+                }
+                Link=Link.next;
+            }
+
+            return true;
+        }
+
 }
