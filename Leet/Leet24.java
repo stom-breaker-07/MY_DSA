a2d3fdfb976b33402ba6b50f8b418d027040619b
stom-breaker-07 <chinmay.l10m8@gmail.com>
2025-05-14 20:44:33 +0530

    May 14

---
+++ Leet/Leet24.java
@@ -0,0 +1,27 @@
+package MY_DSA.Leet;
+
+public class Leet24 {
+      public class ListNode {
+          int val;
+          ListNode next;
+          ListNode() {}
+          ListNode(int val) { this.val = val; }
+          ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+      }
+      public ListNode swapPairs(ListNode head) {
+            if (head == null || head.next == null) return head;
+
+            ListNode current = head;
+
+            while (current != null && current.next != null) {
+
+                int temp = current.val;
+                current.val = current.next.val;
+                current.next.val = temp;
+
+                current = current.next.next;
+            }
+
+            return head;
+      }
+}
