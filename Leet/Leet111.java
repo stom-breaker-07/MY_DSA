a0f618382803d7d93e60a4f50614a2ac3e1e3a3f
stom-breaker-07 <chinmay.l10m8@gmail.com>
2025-06-04 22:39:20 +0530

    Jun04

---
+++ Leet/Leet111.java
@@ -0,0 +1,26 @@
+package MY_DSA.Leet;
+
+public class Leet111 {
+
+     public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+    class Solution {
+        public int min=0;
+        public int minDepth(TreeNode root) {
+            if(root == null) return 0;
+            if(root.left==null) return 1 + minDepth(root.right);
+            else if(root.right==null) return 1 + minDepth(root.left);
+            return 1 + Math.min(minDepth(root.left),minDepth(root.right));
+        }
+    }
+}
