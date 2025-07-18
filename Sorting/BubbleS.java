e9ebddc34af89070b8a0edc99569729777c06860
stom-breaker-07 <chinmay.l10m8@gmail.com>
2025-07-18 12:25:56 +0530

    Revesion 1

--- Sorting/BubbleS.java
+++ Sorting/BubbleS.java
@@ -3,15 +3,14 @@ package MY_DSA.Sorting;
 public class BubbleS {
     public static void bubbleSort(int arr[]) {
         int len=arr.length;
-        while(len>0){
-            for(int i=0;i<len-1;i++){
-                if(arr[i]>arr[i+1]){
-                    int temp=arr[i];
-                    arr[i]=arr[i+1];
-                    arr[i+1]=temp;
+        for(int i=0;i<len-1;i++){
+            for(int j=0;j<len-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
                 }
             }
-            len--;
         }
     }
 }
