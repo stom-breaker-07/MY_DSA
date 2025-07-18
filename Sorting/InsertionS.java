05988061faca47079ef2c6faafc2da216a63bf89
stom-breaker-07 <chinmay.l10m8@gmail.com>
2025-07-18 12:59:14 +0530

    Revesion 1

--- Sorting/InsertionS.java
+++ Sorting/InsertionS.java
@@ -2,14 +2,14 @@ package MY_DSA.Sorting;
 
 public class InsertionS {
     public void insertionSort(int arr[]) {
-        for(int i=1;i<arr.length;i++){
+        // code here
+        int len=arr.length;
+        for(int i=0;i<len-1;i++){
             int j=i;
-            while(j>0){
-                if(arr[j-1]>arr[j]){
-                    int temp=arr[j];
-                    arr[j]=arr[j-1];
-                    arr[j-1]=temp;
-                }
+            while(j>=0 && arr[j+1]<arr[j]){
+                int temp=arr[j+1];
+                arr[j+1]=arr[j];
+                arr[j]=temp;
                 j--;
             }
         }
