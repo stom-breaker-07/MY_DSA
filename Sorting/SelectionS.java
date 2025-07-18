23cfa8ef412e01f9db215b09b992cea25729e42e
stom-breaker-07 <chinmay.l10m8@gmail.com>
2025-07-18 12:40:07 +0530

    Revesion 1

--- Sorting/SelectionS.java
+++ Sorting/SelectionS.java
@@ -1,25 +1,19 @@
 package MY_DSA.Sorting;
 
-import java.util.Arrays;
-
 public class SelectionS {
-    public void SelectionS(int[] arr ){
+    void selectionSort(int[] arr) {
+        // code here
         int len=arr.length;
-        for(int i=0;i<len-1;i++) {
-            for (int j = i + 1; j < len; j++) {
-                if (arr[i] > arr[j]) {
-                   int temp =arr[i];
-                   arr[i]=arr[j];
-                   arr[j]=temp;
+        for(int i=0;i<len-1;i++){
+            int min=i;
+            for(int j=i+1;j<len;j++){
+                if(arr[min]>arr[j]){
+                    min=j;
                 }
             }
+            int temp=arr[min];
+            arr[min]=arr[i];
+            arr[i]=temp;
         }
-    }
-
-    public static void main(String[] args) {
-        int[] a={2,3,1,5,2,5,2};
-        SelectionS ss=new SelectionS();
-        ss.SelectionS(a);
-        System.out.println(Arrays.toString(a));
     }
 }
