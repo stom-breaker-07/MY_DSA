06a6437da87b7d02066b80d129941d0b6046fd14
stom-breaker-07 <chinmay.l10m8@gmail.com>
2025-08-26 00:00:09 +0530

    Aug25

---
+++ Leet/Leet2410.java
@@ -0,0 +1,19 @@
+package MY_DSA.Leet;
+
+public class Leet2410 {
+    public int matchPlayersAndTrainers(int[] players, int[] trainers) {
+        Arrays.sort(players);
+        Arrays.sort(trainers);
+        int i=0,j=0;
+        while(i<players.length && j<trainers.length){
+            if(players[i] <= trainers[j]){
+                i++;
+                j++;
+            }
+            else{
+                j++;
+            }
+        }
+        return i;
+    }
+}
