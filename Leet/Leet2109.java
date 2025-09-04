2faaa28abf4b2a060c643ec88f0409ff84daec12
stom-breaker-07 <chinmay.l10m8@gmail.com>
2025-09-04 20:48:36 +0530

    Sep04

---
+++ Leet/Leet2109.java
@@ -0,0 +1,16 @@
+package MY_DSA.Leet;
+
+public class Leet2109 {
+    public String addSpaces(String s, int[] spaces) {
+        StringBuilder sb=new StringBuilder();
+        int j=0;
+        for(int i=0;i<s.length();i++){
+            if(j<spaces.length && i==spaces[j]){
+                sb.append(" ");
+                j++;
+            }
+            sb.append(s.charAt(i));
+        }
+        return sb.toString();
+    }
+}
