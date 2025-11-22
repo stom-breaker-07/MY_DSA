855bf5340abefb934f0642e97f5187447b487e90
stom-breaker-07 <chinmay.l10m8@gmail.com>
2025-11-22 18:08:11 +0530

    Nov22

---
+++ Leet/Leet636.java
@@ -0,0 +1,31 @@
+package MY_DSA.Leet;
+
+import java.util.List;
+import java.util.Stack;
+
+public class Leet636 {
+    public int[] exclusiveTime(int n, List<String> logs) {
+        int[] result = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        int prevTime = 0;
+        for (String log : logs) {
+            String[] parts = log.split(":");
+            int id = Integer.parseInt(parts[0]);
+            String type = parts[1];
+            int time = Integer.parseInt(parts[2]);
+            if (type.equals("start")) {
+                if (!stack.isEmpty()) {
+                    result[stack.peek()] += time - prevTime;
+                }
+                stack.push(id);
+                prevTime = time;
+            }
+            else {
+                result[stack.pop()] += time - prevTime + 1;
+                prevTime = time + 1;
+            }
+        }
+        return result;
+    }
+}
+
