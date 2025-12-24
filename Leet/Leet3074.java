e5282fad118ae92cae44b76f27ee329da2fc0627
stom-breaker-07 <chinmay.l10m8@gmail.com>
2025-12-24 23:23:35 +0530

    Dec24

---
+++ Leet/Leet3074.java
@@ -0,0 +1,17 @@
+package MY_DSA.Leet;
+
+public class Leet3074 {
+    public int minimumBoxes(int[] apple, int[] capacity) {
+        int Totalpack=0;
+        int m=capacity.length;
+        for(int num :apple){
+            Totalpack+= num;
+        }
+        Arrays.sort(capacity);
+        int idx=m-1;
+        while(idx >=0 && Totalpack>0){
+            Totalpack-=capacity[idx--];
+        }
+        return m-(idx+1);
+    }
+}
