8780c33a5d6774fe0fc6102db1bd746cb67102b2
stom-breaker-07 <chinmay.l10m8@gmail.com>
2026-01-12 10:03:52 +0530

    Get Length

---
+++ CodeChef/CCFindtheLengthoftheLinkedList.java
@@ -0,0 +1,19 @@
+package MY_DSA.CodeChef;
+
+public class CCFindtheLengthoftheLinkedList {
+    class Node
+    {
+        int data;
+        CCdetectCycle.Node next;
+        Node(int d) {data = d; next = null; }
+    }
+    public static int getLength(Node head) {
+        int count=0;
+        Node Current=head;
+        while(Current!=null){
+            Current=Current.next;
+            count++;
+        }
+        return count;
+    }
+}
