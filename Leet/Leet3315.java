a321732f66b5ab9154b518b9474fc3c6f9fc1917
stom-breaker-07 <chinmay.l10m8@gmail.com>
2026-01-21 22:42:03 +0530

    day22

---
+++ Leet/Leet3315.java
@@ -0,0 +1,19 @@
+package MY_DSA.Leet;
+
+public class Leet3315 {
+    public int[] minBitwiseArray(List<Integer> nums) {
+        int len=nums.size();
+        int[] ans=new int[len];
+        for(int i=0;i<len;i++){
+            int a=nums.get(i);
+            int b=a+1;
+            if(a==2){
+                ans[i]=-1;
+            }
+            else{
+                ans[i]= a-((b)&(-b))/2;
+            }
+        }
+        return ans;
+    }
+}
