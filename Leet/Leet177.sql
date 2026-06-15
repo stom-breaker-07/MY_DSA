8f1e0f9453ffec42939295077164989c43f5f2e9
stom-breaker-07 <chinmay.l10m8@gmail.com>
2026-06-15 21:10:17 +0530

    SQL

---
+++ Leet/Leet177.sql
@@ -0,0 +1,10 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  SET N = N-1 ;
+  RETURN (
+      select Distinct salary
+      from Employee
+      order by salary desc
+      limit 1 offset N
+  );
+END
