5c76d5016c1b82949ecc1bda65b9b96c7198f8d3
stom-breaker-07 <chinmay.l10m8@gmail.com>
2026-06-17 14:23:44 +0530

    SQL

---
+++ Leet/Leet184.sql
@@ -0,0 +1,4 @@
+select Department , Employee  , Salary
+from (select d.name as Department, e.name as Employee, e.salary as Salary  , dense_rank() over(partition by d.name order by salary desc ) as rnk
+         from Employee e join Department d on e.departmentId=d.id )
+as temp where rnk=1 ;
