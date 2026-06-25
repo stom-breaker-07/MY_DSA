e65b6de4adefb43c6ddd1b3ff4c47e753ee671d2
stom-breaker-07 <chinmay.l10m8@gmail.com>
2026-06-25 11:07:26 +0530

    SQL

---
+++ Leet/Leet1327.sql
@@ -0,0 +1,8 @@
+select p.product_name ,SUM(o.unit) as unit
+from Products p
+join Orders o
+on p.product_id=o.product_id
+where o.order_date
+between '2020-02-01' and '2020-02-29'
+group by p.product_name
+Having SUM(o.unit) >= 100 ;
